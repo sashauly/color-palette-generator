907c679c07fb4b5f9cbe4fb29469a701d1311ec8
sashauly <77851963+sashauly@users.noreply.github.com>
2025-04-19 20:32:03 +0300

    add: react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,21 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const ReactCompilerConfig = {
+  /* ... */
+};
+
 // https://vite.dev/config/
 export default defineConfig({
   base: "/color-palette-generator/",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

8b5578ebdde5ae2d9df8b2cc2607c7e84ca7c593
sashauly <77851963+sashauly@users.noreply.github.com>
2025-04-28 12:52:14 +0300

    make pwa out of it

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 const ReactCompilerConfig = {
   /* ... */
@@ -17,6 +18,52 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,json}"],
+      },
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon-180x180.png",
+        "artist-palette.svg",
+      ],
+      manifest: {
+        name: "Palette Generator",
+        short_name: "Palette",
+        description: "A PWA for generating color palettes",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        categories: ["utilities"],
+      },
+    }),
   ],
   resolve: {
     alias: {

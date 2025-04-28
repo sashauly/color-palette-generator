05ed8fd22fa6bd05a8c4aa84b0a04219894cf294
sashauly <sashauly@users.noreply.github.com>
2025-04-28 09:54:57 +0000

    deploy: 8b5578ebdde5ae2d9df8b2cc2607c7e84ca7c593

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/color-palette-generator/sw.js', { scope: '/color-palette-generator/' })})}
